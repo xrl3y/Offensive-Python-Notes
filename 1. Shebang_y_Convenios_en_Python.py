f78d096e60205efc54a7869cef68b670b7b1d0f5
Arley Garzon <arstevgg132@gmail.com>
2025-03-10 15:28:15 -0500

    Add files via upload

---
+++ 1. Shebang_y_Convenios_en_Python.py
@@ -0,0 +1,52 @@
+
+# Primeramente para la interpretacion del codigo se le establece que sea interpretado con Python3 en el siguiente formato 
+
+# #!/usr/bin/python3
+
+#no simempre python3 estara en usr bin, asi que otra modalidad que se puede realizar podria ser la siguiente:
+
+# #!/usr/bin/env python3
+
+
+#La siguiente funcion define una estructura de inicio de codigo en un lineamiento de Funcion Principal
+
+if __name__ == '__main__':
+
+    print("Hola mundo")
+
+# Existe un pequeño convenio en el nombramiento de la declaracion de las variables 
+
+#________________________________________________________________________________________#
+
+
+#def comprobacionEstadoApi():
+
+    # lower Camel Case 
+
+# class TwitterApi:
+
+    # Upper Camel Case
+
+# PEP 8
+
+# def comprobar_estado_api():
+
+    #Snake_case 
+
+
+# Declaraacion de Variables de la siguiente forma:
+
+# SCREAMING_SNAKE_CASE 
+
+VERSION_API = 1
+URL_API = "https://hack4u.io"
+
+#Algunas Peculiaridades a la hora de la aplicacion de letras podrian ser las siguientes : l O I 
+
+
+# Para variables protegidas se puede hacer lo siguiente, variables no publicas:
+
+_protegido = "Estoy protegido" 
+
+
+
