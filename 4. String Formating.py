7e26ef1ce54f7130c652cc73c2687f29eee6db21
Arley Garzon <arstevgg132@gmail.com>
2025-03-17 10:05:49 -0500

    Add files via upload

---
+++ 4. String Formating.py
@@ -0,0 +1,26 @@
+# Convertir una serie de datos en un formato especifico 
+
+
+name = "S4vitar"
+rol = "programador"
+
+print("Hola mi nombres es " + name)
+
+# Sustitucion de una o mas variables dentro de la misma cadena
+
+print(" Hola mi nombre es %s y soy un %s" % (name,rol) )
+
+# Segun el tipo de dato es el tipo de porcentaje que tienes que colocar, ejemplificando por ejemplo tipos de datos
+
+# %d ENTERO
+# %f FLOAT
+# %s String
+
+# Otra forma de hacer la misma secuencia tambien podria ser la siguiente:
+
+print("Hola soy {}!".format(name))
+
+# Sustitucion de la variable correspondiente de la siguiente forma 
+
+print(f"Hola mi nombre es {name} y soy un {rol}")
+
